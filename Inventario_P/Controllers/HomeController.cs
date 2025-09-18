8ab09f70a75379d949f71ab27bebb1767684712a
Rodpro512 <fajrod512@gmail.com>
2025-09-18 12:34:11 -0600

    Se agrega "Creacion de Productos" y se coloca en el nav

--- Inventario_P/Controllers/HomeController.cs
+++ Inventario_P/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace Inventario_P.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Login()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+       
     }
 }
